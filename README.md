6d5e20ad4a59f2c88a4d7864f644b22dfb750eda
vivdub <viv_dub@fastmail.fm>
2015-02-03 18:17:05 +0530

    Telegram code commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Telegram
 The Telegram project where in we did few modifications
+
+BCD Social Messenger is a Telegram client that makes instant messaging with friends fun, simple, and secur.
+It is Very practical to improve your language skills by chatting;It has all the awesome features of the official Telegram. 
+You can use Telegram and BCD Social at the same time, with the same number or different numbers.
+Our fork is here: https://github.com/Aniways/Telegram
---
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -0,0 +1,18 @@
+/*
+ * This is the source code of Telegram for Android v. 1.3.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2014.
+ */
+
+package org.telegram.messenger;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static int APP_ID = 17770; //obtaion your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "c90c5b8d9e3bfcee38de66d88b1bf55b"; //obtaion your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
+    public static String GCM_SENDER_ID = "760348033672";
+    public static String SEND_LOGS_EMAIL = "email@gmail.com";
+}
